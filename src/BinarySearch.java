09abc66b3746b76f4b8733f93c340c7be3a71649
RadioactiveOmega <Argameing@ya.ru>
2022-08-07 13:54:47 +0300

    Initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public final class BinarySearch <T extends Comparable<T>> {
+
+    private T[] objects;
+
+    public BinarySearch(T[] objects) {
+        this.setObjects(objects);
+    }
+
+    public T[] getObjects() {
+        return Arrays.copyOf(objects, objects.length);
+    }
+
+    public void setObjects(T[] objects) {
+        if (objects == null) throw new NullPointerException("Array may not be null");
+        this.objects = Arrays.copyOf(objects, objects.length);
+    }
+
+    public int search(T elementToSearch) {
+
+        int top = objects.length - 1;
+        int low = 0;
+        int mid;
+
+        while (low <= top) {
+            mid = (top + low) / 2;
+
+            int result = objects[mid].compareTo(elementToSearch);
+
+            if (result < 0) low = mid + 1;
+            else if (result > 0) top = mid - 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
